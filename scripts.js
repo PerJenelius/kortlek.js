b8b37e92034e4b94ee9ea77a1f49c910ba7e54cd
Per Jenelius <per.jenelius@gmail.com>
2022-07-12 14:47:11 +0200

    Enabled strict mode

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
+'use strict';
+
 const suits = [
     'hearts', 'spades', 'diamonds', 'clubs'
-],
-values = [
+];
+const values = [
     '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'
 ];
 
 let sortedDeck = [],
-shuffledDeck = [];
+    shuffledDeck = [];
 
 const main = () => {
     clearTable();
